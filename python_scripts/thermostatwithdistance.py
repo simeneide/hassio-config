a6a18159d62e5977944903cdbdadd82d8a7194ab
root <root@core-configurator>
2018-11-12 18:10:58 +0100

    push target temp as service - modify nattesenking to stop at 5'o clock

--- python_scripts/thermostatwithdistance.py
+++ python_scripts/thermostatwithdistance.py
@@ -63,13 +63,14 @@ for device_id in ['simen_pixel', 'kamilla_samsung']:
     device_pos = (device_lat, device_lon)
     home_pos = (61.2255604, 7.0982758)
     d = distance(device_pos, home_pos)
-    dist_from_home[device_id] = round(d,3)
+    dist_from_home[device_id] = round(d,2)
     
 dist_from_home['minimum'] = calc_min(dist_from_home.values())
 
 # Update distance states:
 for device_id, value in dist_from_home.items():
-    hass.states.set("sensor.disthome_{}".format(device_id), value)
+    if value is not None:
+        hass.states.set("sensor.disthome_{}".format(device_id), value)
 
 #######################
 ### SET THERMOSTATS ###
@@ -85,7 +86,7 @@ for room in rooms:
     
     # Get input set target temp for room:
     ### HOUR FILTER ###
-    if (hour < 22) & (hour > 6):
+    if (hour < 22) & (hour >= 5):
         target_temp = float(hass.states.get("input_number." + sensor_name).state)
     else:
         target_temp = target_temp_away
@@ -98,6 +99,10 @@ for room in rooms:
     
     final_target_temp = calc_max([target_temp_away, target_temp - round(math.sqrt(dist),1) ])
     if final_target_temp is not None:
-        hass.states.set("climate." + room, target_temp, {"temperature" : final_target_temp})
+        id = "climate." + room
+        hass.services.call(domain = "climate", service = "set_temperature", 
+            service_data = {"temperature" : final_target_temp, "entity_id" : id
+        })
+        #hass.states.set(id, hass.states.get(id).state, {"temperature" : final_target_temp})
         logger.info("hour {}: For room {}, distance {} km, set temperture to {}".format(hour, room, dist, final_target_temp))
     
